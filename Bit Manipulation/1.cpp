245c4db341028bb7e3a8dde6f74fb45e76e1902f
Divyang Bagla <bagladivyang03@gmail.com>
2021-06-13 13:00:00 +0530

    add bit manipulation qs

---
+++ Bit Manipulation/1.cpp
@@ -0,0 +1,48 @@
+// Given a number N and a bit number K, check if Kth bit of N is set or not. A bit is called set if it is 1. Position of set bit '1' should be indexed starting with 0 from LSB side in binary representation of the number.
+
+
+
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+
+class Solution
+{
+    public:
+    // Function to check if Kth bit is set or not.
+    bool checkKthBit(int n, int k)
+    {
+        if(n & (1<<k))
+            return 1;
+        return 0;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver Code
+int main()
+{
+	int t;
+	cin>>t;//taking testcases
+	while(t--)
+	{
+	    long long n;
+	    cin>>n;//input n
+	    int k;
+	    cin>>k;//bit number k
+	    Solution obj;
+	    if(obj.checkKthBit(n, k))
+	        cout << "Yes" << endl;
+	    else
+	        cout << "No" << endl;
+	}
+	return 0;
+}  // } Driver Code Ends
---
+++ Bit Manipulation/2.cpp
@@ -0,0 +1,34 @@
+// Given a positive integer N, print count of set bits in it. 
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    int setBits(int N) {
+        int res =0;
+        while(N > 0){
+            N = (N&(N-1));
+            res++;
+        }
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
